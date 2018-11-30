55da0d400b3c718075acdcd5c1e2f8f634fcc95d
cerise612 <developerdanielle12@gmail.com>
2018-11-29 23:29:29 -0700

    update day 2

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,3 +1,13 @@
+ // Load cookie library
+ $.getScript('https://cdn.rawgit.com/js-cookie/js-cookie/v2.1.2/src/js.cookie.js', function() {
+
+    // If cookie found (already shown)
+    if(Cookies.get('notice') !== undefined) {
+
+      // Hide cookie notice (change jQuery selector to match your own)
+      $('.modal-wrapper').remove();
+    }
+
 $.fn.isInViewport = function() {
     var elementTop = $(this).offset().top;
     var elementBottom = elementTop + $(this).outerHeight();
@@ -17,9 +27,21 @@ $.fn.isInViewport = function() {
         $('#myModal').hide();
       }
     });
+    // On button click
+    $('#modalCloseButton').click(function() {
+        // Calculate when you want to display the notice again (change 15 to number of minutes you want)
+        var expireTime = new Date(new Date().getTime() + 60000 * 60 * 24 * 60);
+        // Set this cookie
+        Cookies.set('notice', 'shown', { expires: expireTime });
+      });
+    });
   });
 
   $('#myModal').modal({ backdrop: 'static' }, 'show');
 
-
-
+  $ (document).ready(function() {   
+    if ($ .cookie("popup_1_2") == null) {
+         $('#modalUser').modal('show');
+    $ .cookie("popup_1_2", "2");
+ }
+});
