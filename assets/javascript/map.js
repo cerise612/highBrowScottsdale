ddb3754de069e20a414c6986730d07aa788b1594
cerise612 <developerdanielle12@gmail.com>
2018-12-04 21:09:48 -0700

    about, main, and contact pages done.

---
+++ assets/javascript/map.js
@@ -0,0 +1,216 @@
+        // Initialize and add the map
+        function initMap() {
+            var styledMapType = new google.maps.StyledMapType(
+                [
+                    {
+                        "featureType": "administrative",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "lightness": -100
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "administrative",
+                        "elementType": "labels.text",
+                        "stylers": [
+                            {
+                                "color": "#000000"
+                            },
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#ffffff"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.natural",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": -8
+                            },
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#ffffff"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "off"
+                            },
+                            {
+                                "hue": "#ff0000"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "simplified"
+                            },
+                            {
+                                "color": "#909090"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "simplified"
+                            },
+                            {
+                                "color": "#000000"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "labels.icon",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "simplified"
+                            },
+                            {
+                                "color": "#b0b0b0"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "hue": "#ff0000"
+                            },
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#d9d9d9"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": 100
+                            },
+                            {
+                                "visibility": "on"
+                            },
+                            {
+                                "color": "#e0e0e0"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "hue": "#000000"
+                            },
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": -100
+                            },
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    }
+                ],
+                {name: 'Styled Map'});
+            // The location of High Brow Scottdale
+            var highBrowScottsdale = {lat: 33.492460, lng: -111.927120};
+            // The map, centered at High Brow Scottdale
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 33.492460, lng: -111.927120},
+                zoom: 18,
+                        });
+            // The marker, positioned at High Brow Scottdale
+            var marker = new google.maps.Marker({
+                position: highBrowScottsdale,
+                icon: URL="assets/icons2/favicon-32x32.png",
+                map: map
+              });
+
+            //Associate the styled map with the MapTypeId and set it to display.
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
+          }
